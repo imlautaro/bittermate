a86676699ec727690502ea790eedc4ef563203a7
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 10:38:42 -0300

    create layout structure

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>
-		<nuxt />
+	<div class="h-screen w-screen overflow-hidden flex flex-col">
+		<div class="h-full flex-1 overflow-y-auto">
+			<nuxt />
+		</div>
 	</div>
 </template>
