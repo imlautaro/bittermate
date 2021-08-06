929d47dd272289807314d0d8f22bd073e053f692
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 12:26:49 -0300

    add button component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,34 @@
+<template>
+	<button
+		class="btn"
+		:class="{
+			'btn--primary': primary != undefined,
+			'btn--secondary': secondary != undefined,
+		}"
+		@click="handleClick"
+	>
+		<phosphor-icons
+			v-if="pending != undefined && pending !== false"
+			class="animate-spin"
+			name="spinner"
+		/>
+		<slot v-else />
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+	props: ['primary', 'secondary', 'pending'],
+	setup(props, { emit }) {
+		const handleClick = () => {
+			if (props.pending == undefined || props.pending === false) {
+				emit('click')
+			}
+		}
+
+		return { handleClick }
+	},
+})
+</script>
