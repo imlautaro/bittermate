2b32951e50e3cc2411a60a2c2c97fe1d7cfdb10f
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 09:55:57 -0300

    fix babel warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
+	build: {
+		babel: {
+			plugins: [
+				[
+					'@babel/plugin-proposal-private-property-in-object',
+					{ loose: true },
+				],
+			],
+		}, // https://github.com/nuxt/nuxt.js/issues/9224#issuecomment-893263501
+	},
 	buildModules: ['@nuxt/typescript-build'],
 	components: true,
 	srcDir: 'src',
