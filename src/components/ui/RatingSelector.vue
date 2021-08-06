d55100bf7778a30badac989ce6c3fcf5e1eef3fa
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 19:56:50 -0300

    add rating selector component

---
+++ src/components/ui/RatingSelector.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="flex items-center justify-evenly text-primary-600">
+		<button
+			v-for="(star, index) in new Array(stars)"
+			:key="index"
+			@click="$emit('change', index + 1)"
+		>
+			<phosphor-icons :name="index >= value ? 'star' : 'star-fill'" />
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: {
+			default: 5,
+			type: Number,
+		},
+		stars: {
+			default: 5,
+			type: Number,
+		},
+	},
+	model: {
+		prop: 'value',
+		event: 'change',
+	},
+}
+</script>
