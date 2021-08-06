3e596065f114ec453a589f8561736aef57e5ae48
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 10:42:23 -0300

    add app-bar

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+	<nav class="bg-primary-600 shadow py-4 text-white" role="navigation">
+		<div
+			class="container flex items-center justify-center md:justify-between"
+		>
+			<nuxt-link class="font-bold text-2xl" to="/">
+				Bittermate
+			</nuxt-link>
+		</div>
+	</nav>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="h-screen w-screen overflow-hidden flex flex-col">
+		<layout-app-bar />
 		<div class="h-full flex-1 overflow-y-auto">
 			<nuxt />
 		</div>
