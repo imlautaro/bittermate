955aa9c0b56532235c99c62988fb9f75392b8726
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-08-06 13:47:57 -0300

    add alert component

---
+++ src/components/ui/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+	<div
+		class="flex items-center p-4 rounded-lg space-x-4 w-full"
+		:class="`bg-${color}-600 bg-opacity-10 text-${color}-600`"
+	>
+		<phosphor-icons v-if="icon != undefined" :name="icon" />
+		<p>
+			<slot />
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		color: {
+			default: 'amber',
+			type: String,
+		},
+		icon: {
+			default: 'warning',
+			type: String,
+		},
+	},
+}
+</script>
