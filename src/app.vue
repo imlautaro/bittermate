91ddbc80c3f7ab4b03ea58729cf1c2b3a7a245a3
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2022-04-29 18:24:37 -0300

    add google fonts

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,16 @@
 <template>
+	<Head>
+		<Link rel="preconnect" href="https://fonts.googleapis.com" />
+		<Link
+			rel="preconnect"
+			href="https://fonts.gstatic.com"
+			crossorigin="anonymous"
+		/>
+		<Link
+			href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;600;700&family=Lexend:wght@400;600;700&display=swap"
+			rel="stylesheet"
+		/>
+	</Head>
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
---
+++ windi.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+	theme: {
+		fontFamily: {
+			sans: ['"Encode Sans"', 'sans-serif'],
+		},
+	},
+})
