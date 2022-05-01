3217331a808cb8b8cdeebe76d20cf979e098877d
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2022-05-01 12:41:43 -0300

    customize theme

--- windi.config.ts
+++ windi.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'windicss/helpers'
+import colors from 'windicss/colors'
 
 export default defineConfig({
 	theme: {
+		extend: {
+			colors: {
+				gray: colors.neutral,
+			},
+			screens: {
+				nt: { raw: '(hover: hover)' },
+			},
+		},
 		fontFamily: {
 			sans: ['"Encode Sans"', 'sans-serif'],
 		},
